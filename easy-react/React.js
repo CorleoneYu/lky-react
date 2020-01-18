2eb909def26cfbeba826b46ef2be782bb69ecd20
CorleoneYu <364723957@qq.com>
2020-01-19 00:58:15 +0800

    feat: 多子组件功能

--- easy-react/React.js
+++ easy-react/React.js
@@ -1,10 +1,20 @@
-function createElement(parentEle, props, childEle) {
+function createElement(parentEle, props, ...children) {
   if (typeof parentEle === 'function') {
     return parentEle();
   }
 
   const parentElement = document.createElement(parentEle);
-  parentElement.innerHTML = childEle;
+  children.forEach(child => {
+    if (typeof child === 'string') {
+      parentElement.innerHTML += child;
+      return;
+    }
+
+    if (typeof child === 'object') {
+      parentElement.appendChild(child);
+      return;
+    }
+  });
   return parentElement;
 }
 
--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 window.addEventListener('load', function () {
-  const Hello = function () {
-    return React.createElement('div', null, 'Hello World V2.0');
+  const HelloV3 = function () {
+    return React.createElement('div', null, 'Hello World V3.0');
   };
 
-  const helloWorld = React.createElement(Hello, null, null);
-  ReactDOM.render(helloWorld, document.getElementById('root'));
+  const helloWorld1 = React.createElement(HelloV3, null, null);
+  const helloWorld2 = React.createElement(HelloV3, null, null);
+  const divEle = React.createElement('div', null, 'div tag');
+  const parent = React.createElement('div', null, helloWorld1, helloWorld2, divEle, 'text content');
+
+  ReactDOM.render(parent, document.getElementById('root'));
 })
